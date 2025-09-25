ad892a877b55ba23a4064ef08d8279c4a8dc24a6
IT24104090 <IT24104090@my.sliit.lk>
2025-09-25 15:32:47 +0530

    Lab 8

---
+++ Lab Sheet 08/IT24104090.R
@@ -0,0 +1,53 @@
+getwd()
+setwd("C:\\Users\\it24104090\\Desktop\\LAB 8")
+data <- read.table("Exercise - LaptopsWeights.txt", header = TRUE)
+fix(data)
+attach(data)
+
+##Q1
+colnames(data) <- c("Weight")
+
+population <- data$Weight
+popmn <- mean(population)
+popvar <- var(population)
+popsd <- sqrt(popvar)
+
+print(paste("Population Mean:", popmn))
+print(paste("Population SD:", popsd))
+
+
+##Q2
+samples <- c()
+n <- c()
+
+for (i in 1:25){
+  s <- sample(population, 6, replace = TRUE)
+  samples <- cbind(samples, s)
+  n <- c(n, paste('s',i))
+}
+
+colnames(samples) = n
+
+s.means <- apply(samples, 2, mean)
+s.vars <- apply(samples, 2, var)
+s.sd <- sqrt(s.vars)
+
+print(paste("Sample Mean:", s.means))
+print(paste("Sample SD:", s.sd))
+
+##Q3
+samplemean <- mean(s.means)
+samplevars <- var(s.means)
+samplesd <- sqrt(samplevars)
+
+popmn
+samplemean
+
+truevar = popsd / 6
+samplesd
+
+truevar = popvar/6
+samplevars
+
+truesd<-sqrt(truevar)
+samplesd
